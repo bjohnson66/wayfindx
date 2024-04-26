798d05024a0c73f3d18fcdc96ca22f057b66a724
bjohnson66 <bcj11@me.com>
2024-04-25 22:42:36 -0500

    SD Card Foundation Work
    * Updated LCD library to use different pins
    * Updated circuit diagram Draw file
    * updated proposal file to match new diagram

--- wfx_sw/wfx_sw/lib/lcd.h
+++ wfx_sw/wfx_sw/lib/lcd.h
@@ -158,19 +158,19 @@
 #define LCD_RS_PORT      PORTB        /**< port for RS line         */
 #endif
 #ifndef LCD_RS_PIN
-#define LCD_RS_PIN       5            /**< pin  for RS line         */
+#define LCD_RS_PIN       1            /**< pin  for RS line         */
 #endif
 #ifndef LCD_RW_PORT
 #define LCD_RW_PORT      PORTB     /**< port for RW line         */
 #endif
 #ifndef LCD_RW_PIN
-#define LCD_RW_PIN       4            /**< pin  for RW line         */
+#define LCD_RW_PIN       0            /**< pin  for RW line         */
 #endif
 #ifndef LCD_E_PORT
-#define LCD_E_PORT       PORTB     /**< port for Enable line     */
+#define LCD_E_PORT       PORTD     /**< port for Enable line     */
 #endif
 #ifndef LCD_E_PIN
-#define LCD_E_PIN        3            /**< pin  for Enable line     */
+#define LCD_E_PIN        7            /**< pin  for Enable line     */
 #endif
 
 #elif defined(__AVR_AT90S4414__) || defined(__AVR_AT90S8515__) || defined(__AVR_ATmega64__) || \
